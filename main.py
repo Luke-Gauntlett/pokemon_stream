9765b71e6358d7b6f808847d7872cd6b166b2ccf
Adedoyin Adepetun <doyinadepetun@gmail.com>
2025-08-04 13:28:55 +0100

    refactored to work with Comfort_branch

--- main.py
+++ main.py
@@ -2,105 +2,117 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+df = pd.read_csv('pokemon.csv')
+df = df.iloc[:, :24]
+df = df.drop(columns=['german_name', 'japanese_name', 'type_number', 'type_2'])
+df = df.rename(columns={'Unnamed: 0': 'csv_index'})
+df = df.set_index('csv_index')
 
-# Set the app title
-st.title("Pokemon Comparisons")
-# Load the Pokemon dataset
-df = pd.read_csv("./pokemon.csv")
-# # Test
-# st.write(df.head(10))
+df['dup_count'] = df.groupby('pokedex_number').cumcount()
+
+df['new_pokedex'] = df.apply(
+    lambda row: (
+        f"{str(row['pokedex_number']).zfill(3)}_f{row['dup_count']+1}"
+        if row['dup_count'] > 0
+        else str(row['pokedex_number']).zfill(3)
+    ),
+    axis=1
+)
+
+df['Pokemon'] = df.apply(
+                    lambda row: f"{row['pokedex_number']:03d} - {row['name']}",
+                    axis=1)
+
+
+def get_pokemon_image_url(index):
+    # Format index as a 3-digit string (e.g., 001, 025, 150)
+    index_str = str(index).zfill(3)
+    return (
+        f"https://www.pokemon.com/static-assets/content-assets/"
+        f"cms2/img/pokedex/full/{index_str}.png"
+    )
+
+
+st.title("Pokémon Image")
+
+selected = st.selectbox("Choose Pokémon", df['Pokemon'])
+
+image_index = df.loc[df['Pokemon'] == selected, 'new_pokedex'].values[0]
+
+img_url = (
+    f"https://www.pokemon.com/static-assets/content-assets/"
+    f"cms2/img/pokedex/full/{image_index}.png"
+)
+
+st.image(img_url, caption=f"{selected}", use_container_width=True)
+
 
 # Get user input
-pokedex_number = st.number_input(
-                            "Enter a Pokedex number (e.g., 6 for Charizard):",
-                            min_value=1,
-                            step=1
-                            )
+pokemon_name = df.loc[df['Pokemon'] == selected, 'name'].values[0]
 
 # Filter to get the selected pokemon
-matching_pokemon = df[df['pokedex_number'] == pokedex_number]
+matching_pokemon = df[df['name'] == pokemon_name]
 
-# This is done to make sure the code does not run when the pokedex is invalid
-selected_pokemon = None
+# Store the details of the matched pokemon in a series
+selected_pokemon = matching_pokemon.iloc[0]
 
-# Write an error is Pokemon isn't found
-if matching_pokemon.empty:
-    st.warning("No Pokemon found with that Pokemon number.")
 
-elif len(matching_pokemon) == 1:
-    selected_pokemon = matching_pokemon.iloc[0]
+# Get the name and height for the pokemon and store in a new DataFrame
+main_pokemon_name = selected_pokemon['name'].strip()
+main_pokemon_weight = selected_pokemon['weight_kg']
+main_pokemon_dict = {
+    "name": main_pokemon_name,
+    "weight_kg": main_pokemon_weight
+}
+main_pokemon_df = pd.DataFrame([main_pokemon_dict])
 
-# Handle multiple matches (e.g., Charizard, Mega Charizard X, etc.)
-else:
-    form_options = matching_pokemon['name'].unique().tolist()
-    selected_form = st.selectbox(
-                            "Multiple Pokemon forms found. Please choose one:",
-                            form_options
-                            )
-    selected_row = matching_pokemon[
-                            matching_pokemon['name'] == selected_form]
-    if not selected_row.empty:
-        selected_pokemon = selected_row.iloc[0]
-    else:
-        st.error("Something went wrong selecting the form.")
+# Get a random selection of other pokemon and store in a new DataFrame
+name_col = "name"
+weight_col = "weight_kg"
 
-if selected_pokemon is not None:
-    # Get the name and height for the pokemon and store in a new DataFrame
-    main_pokemon_name = selected_pokemon['name'].strip()
-    main_pokemon_weight = selected_pokemon['weight_kg']
-    main_pokemon_dict = {
-        "name": main_pokemon_name,
-        "weight_kg": main_pokemon_weight
-    }
-    main_pokemon_df = pd.DataFrame([main_pokemon_dict])
+# Drop any columns with missing name and height
+df_cleaned = df.dropna(subset=[name_col, weight_col])
 
-    # Get a random selection of other pokemon and store in a new DataFrame
-    name_col = "name"
-    weight_col = "weight_kg"
+# Set the number of randomly selected pokemon
+num_selection = 5
 
-    # Drop any columns with missing name and height
-    df_cleaned = df.dropna(subset=[name_col, weight_col])
+# Select the pokemon from the dataframe
+randomly_selected_df = df_cleaned[[name_col, weight_col]].sample(
+                                                    n=num_selection,
+                                                    random_state=5
+                                                )
 
-    # Set the number of randomly selected pokemon
-    num_selection = 5
+# Combine both dataframes
+combined_df = pd.concat([
+                    main_pokemon_df,
+                    randomly_selected_df],
+                    ignore_index=True)
+name_order = combined_df['name'].tolist()
 
-    # Select the pokemon from the dataframe
-    randomly_selected_df = df_cleaned[[name_col, weight_col]].sample(
-                                                        n=num_selection,
-                                                        random_state=42
-                                                    )
+# Create a new column to format height text
+combined_df['weight_text'] = combined_df['weight_kg'].astype(str) + 'kg'
 
-    # Combine both dataframes
-    combined_df = pd.concat([
-                        main_pokemon_df,
-                        randomly_selected_df],
-                        ignore_index=True)
-    name_order = combined_df['name'].tolist()
-
-    # Create a new column to format height text
-    combined_df['weight_text'] = combined_df['weight_kg'].astype(str) + 'kg'
-
-    # Plot the graph
-    fig = px.bar(combined_df,
-                 x='weight_kg',
-                 y='name',
-                 title=(
-                     f"Comparison of {main_pokemon_name}'s Weight "
-                     "to Other Pokemon"
-                 ),
-                 labels={
-                            'name': 'Pokemon Name',
-                            'weight_kg': 'Pokemon Weight (kg)'
-                        },
-                 text='weight_text',
-                 orientation='h',
-                 color='name'
-                 )
-    fig.update_layout(
-        yaxis=dict(
-            categoryorder='array',
-            categoryarray=name_order[::-1]
-        ),
-        showlegend=False
-    )
-    st.plotly_chart(fig)
+# Plot the graph
+fig = px.bar(combined_df,
+             x='weight_kg',
+             y='name',
+             title=(
+                    f"Comparison of {main_pokemon_name}'s Weight "
+                    "to Other Pokemon"
+                ),
+             labels={
+                        'name': 'Pokemon Name',
+                        'weight_kg': 'Pokemon Weight (kg)'
+                    },
+             text='weight_text',
+             orientation='h',
+             color='name'
+             )
+fig.update_layout(
+    yaxis=dict(
+        categoryorder='array',
+        categoryarray=name_order[::-1]
+    ),
+    showlegend=False
+)
+st.plotly_chart(fig)
